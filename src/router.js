60e173cd807dc78913f9566802938a1329677a5f
rido zaen <ridozaen@gmail.com>
2018-06-28 18:49:35 +0700

    added room for join game

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Rooms from './views/Rooms.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/rooms',
+      name: 'rooms',
+      component: Rooms
     }
   ]
 })
---
+++ src/views/Rooms.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-sm-6 mx-auto py-3">
+            <!-- card -->
+            <div class="card border-warning h-100">
+                <div class="card-img-top">
+                    <img src="@/images/field.jpg" class="img-fluid mx-auto d-block" alt="card image 3">
+                </div>
+                <div class="card-body d-flex flex-column p-4">
+                    <p class="text-justify">Varius azdipiscing elit. Duis pharetra, ovi lipsim diro?</p>
+                    <a href="" data-target="#largeModal" data-toggle="modal" class="btn btn-outline-warning btn-lg btn-block text-truncate mt-auto">Join Room</a>
+                </div>
+            </div>
+            <!-- card -->
+        </div>
+        <div class="col-lg-3 col-sm-6 mx-auto py-3">
+                        <!-- card -->
+            <div class="card border-warning h-100">
+                <div class="card-img-top">
+                    <img src="@/images/field.jpg" class="img-fluid mx-auto d-block" alt="card image 3">
+                </div>
+                <div class="card-body d-flex flex-column p-4">
+                    <p class="text-justify">Varius azdipiscing elit. Duis pharetra, ovi lipsim diro?</p>
+                    <a href="" data-target="#largeModal" data-toggle="modal" class="btn btn-outline-warning btn-lg btn-block text-truncate mt-auto">Join Room</a>
+                </div>
+            </div>
+            <!-- card -->
+        </div>
+        <div class="col-lg-3 col-sm-6 mx-auto py-3">
+                        <!-- card -->
+            <div class="card border-warning h-100">
+                <div class="card-img-top">
+                    <img src="@/images/field.jpg" class="img-fluid mx-auto d-block" alt="card image 3">
+                </div>
+                <div class="card-body d-flex flex-column p-4">
+                    <p class="text-justify">Varius azdipiscing elit. Duis pharetra, ovi lipsim diro?</p>
+                    <a href="" data-target="#largeModal" data-toggle="modal" class="btn btn-outline-warning btn-lg btn-block text-truncate mt-auto">Join Room</a>
+                </div>
+            </div>
+            <!-- card -->
+        </div>
+    </div>
+</div>  
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
+
+
