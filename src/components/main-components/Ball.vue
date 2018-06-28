e919a908bad1ad51a2007ebb17e2b0ef2cdf211b
annisaayu <annisaayu569@gmail.com>
2018-06-29 05:27:13 +0700

    add component keeper and ball

---
+++ src/components/main-components/Ball.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="ball">
+    <b-img center src="/img/ball.d91b15e8.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'ball'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/main-components/Keeper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="keeper">
+    <b-img center src="/img/keeper.ee43bae0.png" class="keeper" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'keeper'
+}
+</script>
+
+<style scoped>
+
+</style>
