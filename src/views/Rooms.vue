9f392f636e93a1ffee8cbfc62bb0d8abc7239f6f
rido zaen <ridozaen@gmail.com>
2018-06-29 02:51:44 +0700

    bugfixing: room can only joined one

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -10,8 +10,8 @@
                 <p class="card-text" v-if="roomOne.player_one">Player 1: {{roomOne.player_one.name}} as a {{roomOne.player_one.role}}</p>
                 <p class="card-text" v-if="roomOne.player_two">Player 2: {{roomOne.player_two.name}} as a {{roomOne.player_two.role}}</p>
                 <p class="card-text" v-if="messagesOne">{{messagesOne}}</p>
-                <button class="btn btn-warning" v-if="roomOne.player_one && roomOne.player_two" @click="startGame('one')">Start Game</button>
-                <button class="btn btn-warning" v-else>Join Room</button>
+                <!-- <button class="btn btn-warning" v-if="roomOne.player_one && roomOne.player_two" @click="startGame('one')">Start Game</button> -->
+                <button id="one" class="btn btn-warning" :disabled="(roomOne.player_one && roomOne.player_two) || joinRoom" @click="joinGame('one')">Join Room</button>
               </div>
             </div>
             <!-- card -->
@@ -25,8 +25,8 @@
                 <p class="card-text" v-if="roomTwo.player_one">Player 1: {{roomTwo.player_one.name}} as a {{roomTwo.player_one.role}}</p>
                 <p class="card-text" v-if="roomTwo.player_two">Player 2: {{roomTwo.player_two.name}} as a {{roomTwo.player_two.role}}</p>
                 <p class="card-text" v-if="messagesTwo">{{messagesTwo}}</p>
-                <button class="btn btn-warning" v-if="roomTwo.player_one && roomTwo.player_two" @click="startGame('two')">Start Game</button>
-                <button class="btn btn-warning" v-else>Join Room</button>
+                <!-- <button class="btn btn-warning" v-if="roomTwo.player_one && roomTwo.player_two" @click="startGame('two')">Start Game</button> -->
+                <button id="two" class="btn btn-warning" :disabled="(roomTwo.player_one && roomTwo.player_two) || joinRoom" @click="joinGame('two')">Join Room</button>
               </div>
             </div>
             <!-- card -->
@@ -40,8 +40,7 @@
                 <p class="card-text" v-if="roomThree.player_one">Player 1: {{roomThree.player_one.name}} as a {{roomThree.player_one.role}}</p>
                 <p class="card-text" v-if="roomThree.player_two">Player 2: {{roomThree.player_two.name}} as a {{roomThree.player_two.role}}</p>
                 <p class="card-text" v-if="messagesThree">{{messagesThree}}</p>
-                <button class="btn btn-warning" v-if="roomThree.player_one && roomThree.player_two" @click="startGame('three')">Start Game</button>
-                <button class="btn btn-warning" v-else>Join Room</button>
+                <button id="three" class="btn btn-warning" :disabled="(roomThree.player_one && roomThree.player_two) || joinRoom" @click="joinGame('three')">Join Room</button>
               </div>
             </div>
             <!-- card -->
@@ -55,59 +54,162 @@ export default {
   data() {
     return {
       currName: null,
-      roomOne: {},
-      roomTwo: {},
-      roomThree: {},
+      roomOne: "",
+      roomTwo: "",
+      roomThree: "",
       messagesOne: null,
       messagesTwo: null,
-      messagesThree: null
+      messagesThree: null,
+      joinRoom: false
     };
   },
   methods: {
-    startGame(param) {
+    joinGame(param) {
       console.log(param);
-      this.$router.push(`/rooms/${param}`);
+      const arrRole =['keeper','kicker']
+      let currRole
+      //set to localstorage
+      localStorage.setItem('joinRoom',param)
+      this.joinRoom = true
+      //room one
+      if (param === "one") {
+        if (this.roomOne) {
+          if(this.roomOne.player_one.role === arrRole[0]){
+            currRole = arrRole[1]
+          }else{
+            currRole = arrRole[0]
+          }
+          //let join and start game
+          roomOne.child("player_two").set({
+            name: 'ujang',
+            role: currRole,
+            score:0
+          })
+        } else {
+          console.log("both player doesn't exist");
+          let random = Math.floor(Math.random() * 2)
+          roomOne.child("player_one").set({
+            name: "jajang",
+            role: arrRole[random],
+            score: 0
+          });
+        }
+      }
+      //room two
+      if (param === "two") {
+        if (this.roomTwo) {
+          if(this.roomTwo.player_one.role === arrRole[0]){
+            currRole = arrRole[1]
+          }else{
+            currRole = arrRole[0]
+          }
+          roomTwo.child("player_two").set({
+            name: 'ujang',
+            role: currRole,
+            score:0
+          })
+        } else {
+          console.log("null");
+          let random = Math.floor(Math.random() * 2)
+          roomTwo.child("player_one").set({
+            name: "jajang",
+            role: arrRole[random],
+            score: 0
+          });
+        }
+      }
+      //room three
+      if (param === "three") {
+        if (this.roomThree) {
+          if(this.roomThree.player_one.role === arrRole[0]){
+            currRole = arrRole[1]
+          }else{
+            currRole = arrRole[0]
+          }
+                    //let join and start game
+          roomThree.child("player_two").set({
+            name: 'ujang',
+            role: currRole,
+            score:0
+          })
+        } else {
+          console.log("null");
+          let random = Math.floor(Math.random() * 2)
+          roomThree.child("player_one").set({
+            name: "jajang",
+            role: arrRole[random],
+            score: 0
+          });
+        }
+      }
     }
   },
   mounted() {
     console.log("test mounted");
+    //roomOne
+    if (localStorage.getItem('joinRoom')){
+      this.joinRoom = true
+    }
     roomOne.on("value", snapshot => {
-      console.log(snapshot.val());
+      console.log('roomOne',snapshot.val());
       if (snapshot.val()) {
         if (!snapshot.val().player_two) {
-          this.messages = "Waiting another player...";
+          this.messagesOne = "Waiting another player...";
         } else {
           this.messagesOne = null;
+          if (
+            localStorage.getItem("joinRoom") &&
+            localStorage.getItem("joinRoom") === "one"
+          ) {
+            this.$router.push("/rooms/one");
+          }
         }
         this.roomOne = snapshot.val();
       } else {
         this.messagesOne = "Waiting player";
+        this.roomOne = ''
       }
     });
+
+    //roomTwo
     roomTwo.on("value", snapshot => {
-      console.log(snapshot.val());
+      console.log('roomTwo',snapshot.val());
       if (snapshot.val()) {
         if (!snapshot.val().player_two) {
           this.messagesTwo = "Waiting another player...";
         } else {
           this.messagesTwo = null;
+          if (localStorage.getItem("joinRoom") &&
+            localStorage.getItem("joinRoom") === "two") {
+            this.$router.push("/rooms/two");
+          }
         }
         this.roomTwo = snapshot.val();
       } else {
         this.messagesTwo = "Waiting player";
+        this.roomTwo = ''
       }
     });
+
+    //roomThree
     roomThree.on("value", snapshot => {
-      console.log(snapshot.val());
+      console.log('roomThree',snapshot.val());
       if (snapshot.val()) {
         if (!snapshot.val().player_two) {
           this.messagesThree = "Waiting another player...";
         } else {
           this.messagesThree = null;
+          if (
+            localStorage.getItem("joinRoom") &&
+            localStorage.getItem("joinRoom") === "three"
+          ) {
+            this.$router.push("/rooms/three");
+          }
         }
         this.roomThree = snapshot.val();
       } else {
         this.messagesThree = "Waiting player...";
+        this.roomThree='';
       }
     });
   }
