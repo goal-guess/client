31ac2b7e6b03492736395246c6e671eaf466ede0
annisaayu <annisaayu569@gmail.com>
2018-06-29 09:13:25 +0700

    add methods for keeper

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,9 @@
 <template>
   <b-container>
-    <b-row class="keeper-range">
+    <div class="keeper-range">
       <Keeper></Keeper>
-    </b-row>
-    <b-row align-v="end">
+    </div>
+    <b-row class="fixed-bottom">
         <b-col>Score</b-col>
         <b-col>
           <Ball></Ball>
@@ -11,12 +11,12 @@
         <b-col style="font-size: 0.5rem; color: Dodgerblue;">
           <b-row>
             <b-col></b-col>
-            <b-col><i class="fas fa-caret-square-up fa-10x"></i></b-col>
+            <b-col><i class="fas fa-caret-square-up fa-10x" v-on:click="directionAct('up')"></i></b-col>
             <b-col></b-col>
           </b-row>
           <b-row>
-            <b-col md="3" offset-md="1"><i class="fas fa-caret-square-left fa-10x"></i></b-col>
-            <b-col md="3" offset-md="3"><i class="fas fa-caret-square-right fa-10x"></i></b-col>
+            <b-col md="3" offset-md="1"><i class="fas fa-caret-square-left fa-10x" v-on:click="directionAct('left')"></i></b-col>
+            <b-col md="3" offset-md="3"><i class="fas fa-caret-square-right fa-10x" v-on:click="directionAct('right')"></i></b-col>
           </b-row>
         </b-col>
     </b-row>
@@ -33,6 +33,34 @@ export default {
   components: {
     Ball,
     Keeper
+  },
+  data () {
+    return {
+      players: {
+        player_one: {
+          name: 'Dian',
+          role: 'keeper',
+          score: 0,
+          direction: null
+        },
+        player_two: {
+          name: 'Roni',
+          role: 'kicker',
+          score: 0,
+          direction: null
+        }
+      }
+    }
+  },
+  methods: {
+    directionAct (event) {
+      // console.log(event)
+      for(let key in this.players){
+        if(this.players[key].role == 'keeper') {
+          this.players[key].direction = event
+        }
+      }
+    }
   }
 }
 </script>
